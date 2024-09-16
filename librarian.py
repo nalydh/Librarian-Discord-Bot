c8d913ca3dbf3bf806cadfb4957777433779c94e
nalydh <dhoa0009@student.monash.edu>
2024-09-16 21:06:28 +1000

    Implemented !template function - Bot sends template to the channel the message was sent in - Also deletes the user's !template message for cleanliness.

--- librarian.py
+++ librarian.py
@@ -2,12 +2,20 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import time
 
 load_dotenv("token.env")
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
-
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.command()
+async def template(message):
+    channel = message.channel
+    summary_template = "Reading **(Book Title)** (chapter (**Chapter Number**)):\n# Chapter Title \n## ⭐️ Lessons Learnt\n- Paragraph 1\n- Paragraph 2\n- Paragraph ..."
+    await channel.send(summary_template)
+    await message.message.delete(delay=2)
+
 
 bot.run(DISCORD_TOKEN)
