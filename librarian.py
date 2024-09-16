7045378080afea295b80405f38fd340ecf6f316d
nalydh <dhoa0009@student.monash.edu>
2024-09-16 21:15:50 +1000

    Modified the template function - Now deletes the template message the bot outputs after 10 seconds. - Inserted a warning message to let users know of the deletion.

--- librarian.py
+++ librarian.py
@@ -13,9 +13,13 @@ bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 @bot.command()
 async def template(message):
     channel = message.channel
-    summary_template = "Reading **(Book Title)** (chapter (**Chapter Number**)):\n# Chapter Title \n## ⭐️ Lessons Learnt\n- Paragraph 1\n- Paragraph 2\n- Paragraph ..."
-    await channel.send(summary_template)
-    await message.message.delete(delay=2)
+    summary_template = "\nReading **(Book Title)** (chapter (**Chapter Number**)):\n# Chapter Title \n## ⭐️ Lessons Learnt\n- Paragraph 1\n- Paragraph 2\n- Paragraph ..."
+    warning_message = "**WARNING:** This template will be deleted after __10 seconds__. Copy the text quickly!\n"
+    bot_output_warning = await channel.send(warning_message)
+    bot_output_template = await channel.send(summary_template)
+    await message.message.delete(delay=0.1)
+    await bot_output_warning.delete(delay=10)
+    await bot_output_template.delete(delay=10)
 
 
 bot.run(DISCORD_TOKEN)
